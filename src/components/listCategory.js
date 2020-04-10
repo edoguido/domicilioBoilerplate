0804a0d622b2440df1402e700e87eedaa33191a4
Edoardo Guido <edoardo.guido.ext@accurat.it>
2020-04-10 19:53:57 +0200

    Add sticky positioning for categories

--- src/components/listCategory.js
+++ src/components/listCategory.js
@@ -1,19 +1,16 @@
-import { ListItem } from './lisItem';
+import { ListItem } from "./lisItem";
 
-export const ListCategory = ({ name = '', category = {}}) => {
-	return (
-		<div id={name.replace(' ', '_')} className="relative py-5">
-			<p className="text-3xl md:text-4xl capitalize">
-				{category.icon} {name}
-			</p>
-			<div>
-				{
-					category.data
-						.map(props => (
-							<ListItem key={props.tel} {...props} />
-						))
-				}
-			</div>
-		</div>
-	);
+export const ListCategory = ({ name = "", category = {} }) => {
+   return (
+      <div id={name.replace(" ", "_")} className="relative py-5">
+         <p className="text-3xl md:text-4xl capitalize sticky bgwhite">
+            {category.icon} {name}
+         </p>
+         <div>
+            {category.data.map((props) => (
+               <ListItem key={props.tel} {...props} />
+            ))}
+         </div>
+      </div>
+   );
 };
